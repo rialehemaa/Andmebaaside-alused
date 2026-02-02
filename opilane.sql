fcef5971632cbb011f0c346c608df33ba8910ac7
rialehemaa <dariia.klimuk17@gmail.com>
2026-02-02 16:05:55 +0200

    Update 'perenimi' column to UNIQUE and add records
    Modified the 'perenimi' column to enforce a UNIQUE constraint and added multiple insert statements for new records.

--- opilane.sql
+++ opilane.sql
@@ -5,8 +5,25 @@ use klimukSQL;
 CREATE TABLE opilane(
 opilaneID int PRIMARY KEY identity(1,1),
 eesnimi varchar(25),
-perenimi varchar(30) NOT null,
+perenimi varchar(30) NOT null UNIQUE,
 synniaeg date,
 aadress TEXT,
 kas_opib bit);
+--kuvab tabeli, * - kõik väljad
 SELECT * FROM opilane;
+
+--tabeli kustutamine
+DROP TABLE opilane;
+
+--andmete lisamine tabelisse opilane
+--lisamine 3.kirjet korraga
+INSERT INTO opilane(eesnimi, perenimi,synniaeg,kas_opib)
+VALUES ('Nikita', 'Petrov', '2023-12-12',0);
+('Nikita', 'Alekseev', '2020-11-12',0);
+('Nikita', 'Nikitin', '2021-12-13',0);
+
+
+Select * from opilane;
+
+--muudame tabeli ja lisame piirangu UNIQUE
+ALTER TABLE opilane Alter column perenimi varchar(30) UNIQUE;
